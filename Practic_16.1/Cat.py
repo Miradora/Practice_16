d7b005cbf5c7966126a252a6c18826cab296dac8
Miradora <volskaya.olga.70@mail.ru>
2022-02-09 21:34:10 +0300

    Practic_16.1

---
+++ Practic_16.1/Cat.py
@@ -0,0 +1,11 @@
+from Cats import Cat
+
+Cat_1 = Cat("Барон", "мальчик", 2)
+Cat_2 = Cat("Сэм", "мальчик", 2)
+
+print("Кот:", Cat_1.name, Cat_1.gender, Cat_1.age)
+print("Кот:", Cat_2.name, Cat_2.gender, Cat_2.age)
+
+
+
+
---
+++ rectangle.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def getWidth(self):
+        return self.width
+
+    def getHeight(self):
+        return self.height
+
+    def getArea(self):
+        return self.width * self.height
+
+
+
+
